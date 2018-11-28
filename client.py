417dbeb9dd29615f295c6fdb25617d90749228b1
Talize <talizefq@gmail.com>
2018-11-28 15:01:43 -0200

    Updating client and server functions

--- client.py
+++ client.py
@@ -3,6 +3,10 @@ import socket
 import os
 import _thread
 import time
+import random
+import numpy as np
+from copy import deepcopy
+from os import system
 
 # System parameters
 host = "localhost"
@@ -12,20 +16,89 @@ size = 1024
 # Create connection to server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host,port))
-name = ""
+id = ""
 
-# Initial Localization
-#COLAR AQUI
+# Le arquivo txt e salva em uma matriz de char
+n_linhas = 40
+n_colunas = 120
+dim = (n_linhas, n_colunas)
+mapa_original = np.chararray(dim)
+with open("map.txt") as f:
+    i = 0
+    j = 0
+    while True:
+        c = f.read(1)
+        if not c:
+            break
+        if c != '\n':
+            mapa_original[i][j] = c
+            j = j + 1
+        else:
+            i = i + 1
+            j = 0
+mapa = deepcopy(mapa_original)
 
-def send_message():
+# Movimento aleatório do usuário
+def movimenta(p):
+    x = p[0]
+    y = p[1]
+    x_antes = x
+    y_antes = y
+
+    falhou = False
+    while (x_antes == x and y_antes == y):
+        cont = 0
+        if mapa_original[x-1][y] == b'0':
+            cont = cont+1
+        if mapa_original[x+1][y] == b'0': 
+            cont = cont+1
+        if mapa_original[x][y-1] == b'0':
+            cont = cont+1
+        if mapa_original[x][y+1] == b'0':
+            cont = cont+1
+
+        rnd = random.random()
+        if cont <= 2 and falhou == False:
+            rnd = p[2]
+        
+        #cima
+        if rnd < 0.25:
+            if mapa_original[x-1][y] == b'0':
+                x = x - 1
+        #baixo
+        elif rnd < 0.5:
+            if mapa_original[x+1][y] == b'0': 
+                x = x + 1
+        #esquerda
+        elif rnd < 0.75:
+            if mapa_original[x][y-1] == b'0':
+                y = y - 1
+        #direita
+        else:
+            if mapa_original[x][y+1] == b'0': 
+                y = y + 1
+        falhou = True
+    return x, y, rnd
+
+def send_localization():
+    # Initial Localization
+    x0 = random.randint(0, n_linhas-1)
+    y0 = random.randint(0, n_colunas-1)
+
+    while mapa_original[x0][y0]!=b'0':
+        x0 = random.randint(0, n_linhas-1)
+        y0 = random.randint(0, n_colunas-1)
+    p = (x0, y0, random.random())
+
+    #Manda a localizacao com o id e movimenta
     while True:
         print("Localization")
-        local = input() #NAO VAI SER INPUT, VAI SER A LOCALIZACAO
-        #CODIGO COM TEMPORIZACAO:
-        # é pra ser de tempos em tempos com local recebendo a localizacao depois de ter movimentado
+        local = p
+        p = movimenta(p)
         data = json.dumps({"type": "localization", "payload": {"source": id, "content": local}})
         s.send(data.encode("utf-8"))
-
+        time.sleep(0.05)
+        
 # Request an user ID (must be unique in the system)
 print("What ID do you want to use?")
 while True:
@@ -42,7 +115,7 @@ while True:
         else:
             print("ID already taken. Try another one.")
 
-_thread.start_new_thread(send_message, ())
+_thread.start_new_thread(send_localization, ())
 
 while True:
     raw_data = s.recv(size)
--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 import json
 import socket
 import _thread
+import random
+import time
+import numpy as np
+from copy import deepcopy
+from os import system
 
 # System parameters
 host = "localhost"
@@ -11,13 +16,148 @@ size = 1024
 # Connections dictionary: keys are user IDs, values are connections to clients
 connections = {}
 
+# Le arquivo txt e salva em uma matriz de char
+n_linhas = 40
+n_colunas = 120
+dim = (n_linhas, n_colunas)
+mapa_original = np.chararray(dim)
+with open("map.txt") as f:
+    i = 0
+    j = 0
+    while True:
+        c = f.read(1)
+        if not c:
+            break
+        if c != '\n':
+            mapa_original[i][j] = c
+            j = j + 1
+        else:
+            i = i + 1
+            j = 0
+mapa = deepcopy(mapa_original)
+
+# Matriz de inteiros que indica posicoes mais percorridas
+mapaint = np.zeros(dim)
+for i in range(0, n_linhas):
+    for j in range(0, n_colunas):
+        if mapa_original[i][j] != b'0':
+            mapaint[i][j] = -1
+
+# Acende um bloco de "n_em_volta" casas em todas as direções da posicao
+def acende_luzes_em_volta(p, cont):
+    global mapa
+    if cont >= 1:
+        if mapa[p[0] - 1][p[1]] == b'0':
+            mapa[p[0] - 1][p[1]] = b'+'
+            acende_luzes_em_volta((p[0] - 1, p[1]), cont - 1)
+        if mapa[p[0] + 1][p[1]] == b'0':
+            mapa[p[0] + 1][p[1]] = b'+'
+            acende_luzes_em_volta((p[0] + 1, p[1]), cont - 1)
+        if mapa[p[0]][p[1] - 1] == b'0': 
+            mapa[p[0]][p[1] - 1] = b'+' 
+            acende_luzes_em_volta((p[0], p[1] - 1), cont - 1)
+        if mapa[p[0]][p[1] + 1] == b'0':
+            mapa[p[0]][p[1] + 1] = b'+'  
+            acende_luzes_em_volta((p[0], p[1] + 1), cont - 1)
+
+# Acende o caminho mais percorrido a partir daquela posicao
+def acende_luzes_caminho_otimizado(p, cont):
+    global mapa
+    global mapaint
+    sentido = "cima"
+    maior = mapaint[p[0]-1][p[1]]
+
+    #baixo
+    if maior < mapaint[p[0]+1][p[1]]:
+        sentido = "baixo"
+        maior = mapaint[p[0]+1][p[1]]
+
+    #esquerda
+    if maior < mapaint[p[0]][p[1]-1]:
+        sentido = "esquerda"
+        maior = mapaint[p[0]][p[1]-1]
+
+    #direita
+    if maior < mapaint[p[0]][p[1]+1]:
+        sentido = "direita"
+        maior = mapaint[p[0]][p[1]+1]
+
+    if cont >= 1:
+        if sentido == "cima":
+            if mapa[p[0] - 1][p[1]] != b'P':
+                mapa[p[0] - 1][p[1]] = b'+'
+            acende_luzes_caminho_otimizado((p[0] - 1, p[1]), cont - 1)
+        elif sentido == "baixo":
+            if mapa[p[0] + 1][p[1]] != b'P':
+                mapa[p[0] + 1][p[1]] = b'+'
+            acende_luzes_caminho_otimizado((p[0] + 1, p[1]), cont - 1)
+        elif sentido ==  "esquerda":
+            if mapa[p[0]][p[1] - 1] != b'P': 
+                mapa[p[0]][p[1] - 1] = b'+' 
+            acende_luzes_caminho_otimizado((p[0], p[1] - 1), cont - 1)
+        elif sentido ==  "direita":
+            if mapa[p[0]][p[1] + 1] != b'P':
+                mapa[p[0]][p[1] + 1] = b'+'  
+            acende_luzes_caminho_otimizado((p[0], p[1] + 1), cont - 1)
+
+# Reinicia o mapa
+def reiniciar_mapa():
+    global mapa
+    mapa = deepcopy(mapa_original)
+
+# Print do mapa
+def print_map(matriz):
+    for i in range (0, n_linhas):  
+        for j in range (0, n_colunas):
+            if matriz[i][j] == b'.':
+                print (".", end = '')
+            elif matriz[i][j] == b'0':
+                print (" ", end = '')
+            elif matriz[i][j] == b'P':
+                print ("☺", end = '')
+            elif matriz[i][j] == b'+':
+                print ("+", end = '')
+            else:
+                print ("x", end = '')
+        print() 
+
+# Atualiza mapaint
+def atualiza_mapaint(p):
+    global mapaint
+    mapaint[p[0], p[1]] = mapaint[p[0], p[1]] + 1 
+
+# Ilumina mapa
+def ilumina_mapa():
+    global mapa
+    for i in range(0, n_linhas):
+        for j in range(0, n_colunas):
+            if mapa[i][j] == b'P':
+                p = (i, j)
+                acende_luzes_em_volta(p, 4)
+                acende_luzes_caminho_otimizado(p, 10)
+
+# Rendimento em %
+def calcula_rendimento():
+    global mapa
+    total_luzes = 0
+    luzes_acesas = 0
+    for i in range(0, n_linhas):
+        for j in range(0, n_colunas):
+            if mapa[i][j] == b'+' or mapa[i][j] == b'P':
+                luzes_acesas = luzes_acesas + 1
+                total_luzes = total_luzes + 1
+            elif mapa[i][j] == b'0':
+                total_luzes = total_luzes + 1
+    return 100*(total_luzes-luzes_acesas)/total_luzes
+
+
 def new_client(client, address):
     while True:
         raw_data = client.recv(size)
         if raw_data:
             # Decode the data
             data = json.loads(raw_data)
-            print("SERVER >> Received:", data, "from", address)
+            ##print("SERVER >> Received:", data, "from", address)
 
             # If the data is of type connection, a client is requesting an ID
             # This verifies if the ID has been taken already and, if not, adds it
@@ -38,7 +178,17 @@ def new_client(client, address):
 
             # If the data is of type localization, a client is attempting to send his localization to the server.
             elif data["type"] == "localization":
-                print(data["payload"]["content"])
+                #prints de testes
+                print("recebi do id:", data["payload"]["source"])
+                print("localizacao:", data["payload"]["content"])
+                # itera sobre uma lista que armazena a posicao de cada processo e atualiza todas essas funcoes:
+                #atualiza_mapaint(data["payload"]["content"])
+                #mapa[data["payload"]["content"][0]][data["payload"]["content"][1]] = 'P'
+                #ilumina_mapa()
+                #print_map(mapa)
+                #print("Rendimento = ", "%.1f" % calcula_rendimento(), " % de economia")
+                #reiniciar_mapa()    
+                #time.sleep(0.05)
             
             #PRA TIRAR O CLIENT:        
             #del connections[data["payload"]["source"]]
